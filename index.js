7305dd289c22370138169319f7a7839eaab615c4
Mujib <mujib@Mujibs-MacBook-Pro-2.local>
2024-06-13 08:14:16 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// output statement 
+// input -> process -> output
+
+// System.out.println('Hello world');
+console.log('Hello World');
+
+// compilation + execution = 1step
+// Javac Index.java compilation
+// Java Index executions
---
+++ index2.js
@@ -0,0 +1,16 @@
+// variables
+// memory (RAM), temp
+// memory location
+// declared, initialized
+// var x; // delcaration
+
+// x = 10; // assignment operator = 
+// toggle comment Ctrl+/ or CMD + /
+/*
+    multiple
+    line
+    comments
+    Data types
+*/
+var x = 10;
+console.log(x);
